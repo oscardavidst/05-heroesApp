b6777e7a9fb8eb86506830184d0113855467bd00
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-13 00:11:54 -0500

    Tarea: HeroeTarjetaComponent

---
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+})
+export class HeroeTarjetaComponent {
+  @Input() heroe!: Heroe;
+}
