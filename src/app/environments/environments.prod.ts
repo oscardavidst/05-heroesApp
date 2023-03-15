bbb45cc7925b30d2a2db38c0a2c1da65ff454e11
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-14 22:23:00 -0500

    Variables de entorno

---
+++ src/app/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiEndPoint: 'http://oscar-soto.com/api',
+};
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Heroe } from '../interfaces/heroes.interface';
 import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  private apiUrl: string = environment.apiEndPoint;
+
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.apiUrl}/heroes`);
   }
 
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.apiUrl}/heroes/${id}`);
   }
 }
