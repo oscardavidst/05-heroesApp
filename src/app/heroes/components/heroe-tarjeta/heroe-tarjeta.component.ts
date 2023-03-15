c4d1570a173520b1419574f957f77299d49455b6
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-14 22:04:03 -0500

    Diseño de la pantalla Heroe

--- src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -4,6 +4,13 @@ import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
+  styles: [
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class HeroeTarjetaComponent {
   @Input() heroe!: Heroe;
