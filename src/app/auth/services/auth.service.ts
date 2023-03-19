69491e37356cb9fd807c2b2814ce7eacfee8fd01
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-18 23:28:13 -0500

    Mostrar la informacion del usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,18 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from 'src/app/environments/environments';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Auth } from '../interfaces/auth.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private _auth: Auth | undefined;
   private apiUrl: string = environment.apiEndPoint;
 
   constructor(private http: HttpClient) {}
 
+  get auth() {
+    return { ...this._auth };
+  }
+
   login(id: number): Observable<Auth> {
-    return this.http.get<Auth>(`${this.apiUrl}/usuarios/${id}`);
+    return this.http
+      .get<Auth>(`${this.apiUrl}/usuarios/${id}`)
+      .pipe(tap((resp) => (this._auth = resp)));
   }
 }
